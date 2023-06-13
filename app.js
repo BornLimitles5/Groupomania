ea72e1faac412d753111bb2e0b12f6bc0d818b29
PRFE 6 <prfe6@assofac.org>
2023-06-13 09:42:26 +0200

    Online?

--- app.js
+++ app.js
@@ -49,9 +49,9 @@ handlebarsHelpers({ handlebars: hbs.handlebars });
 
 hbs.registerHelper('ifCond', function(v1, v2, options) {
     if (v1 == v2) {
-      return options.fn(this);
+        return options.fn(this);
     } else {    
-      return options.inverse(this);
+        return options.inverse(this);
     }
 });
 
